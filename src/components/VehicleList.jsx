6e43ec68cbe256b04eb0918c1b00c93cdf1efd11
Pamella Pereto <pamellapereto@gmail.com>
2023-03-03 16:24:31 -0300

    Atualização Projeto Cadastro de Veículos

--- src/components/VehicleList.jsx
+++ src/components/VehicleList.jsx
@@ -26,9 +26,6 @@ export const VehicleList = () => {
 
             <h1 className="my-5 text-center">Veículos cadastrados</h1>
 
-
-            {/*<input type='text' placeholder='Pesquisar' />*/}
-
             {
                 vehicles.length > 0 ? (
 
