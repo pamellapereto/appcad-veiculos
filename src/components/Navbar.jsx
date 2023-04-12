add27b4361d8a38900d7bc2426b3c1e8b3d70fba
Pamella Pereto <pamellapereto@gmail.com>
2023-04-12 11:08:14 -0300

    Atualização

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="/cadastrar-veiculo">Cadastrar novo</a>
+                                <a className="nav-link" href="/cadastrar-veiculo">Cadastrar novo veículo</a>
                             </li>
 
                         </ul>
